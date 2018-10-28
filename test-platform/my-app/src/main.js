4c4e77d5ffd021d8b8991176792fa32e29aeb8e4
stendet <mStendet@gmail.com>
2018-10-28 15:42:39 +0200

    test SSH

--- test-platform/my-app/src/main.js
+++ test-platform/my-app/src/main.js
@@ -3,7 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import i18n from './i18n'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
